17d0415c028f273e5fe7c8efe601d8bda6a6e370
Gene Williams <GeneWillb@Gmail.com>
2018-02-04 12:24:19 -0500

    item info added to a table

--- sweetwater/src/components/ShoppingCart.vue
+++ sweetwater/src/components/ShoppingCart.vue
@@ -1,33 +1,64 @@
 <template>
   <div>
-    <div v-for="item in itemInfo" class="cart">
+    <div class="cart">
       <!--image(that is a link to item), manufacturer ,product name, itemID,-->
-      <div v-repeat="item in itemInfo" class="cartItem">
-        <img :src="item.image" :alt="item.productName"/>
-        {{item.manufacturer}}
-        {{item.productName}}
-        {{item.itemId}}
-        {{item.description}}
-        {{item.quantity}}
-        {{item.price}}
+      <!--<div v-repeat="item in itemInfo" class="cartItem">-->
+      <!--<img :src="item.image" :alt="item.productName"/>-->
+      <!--{{item.manufacturer}}-->
+      <!--{{item.productName}}-->
+      <!--{{item.itemId}}-->
+      <!--{{item.description}}-->
+      <!--{{item.quantity}}-->
+      <!--{{item.price}}-->
       </div>
+      
+      <table class="table">
+        <thead>
+        <tr>
+          <th scope="col">#</th>
+          <th scope="col">Item</th>
+          <th scope="col">Info</th>
+          <th scope="col">Quantity</th>
+          <th scope="col">Price</th>
+        </tr>
+        </thead>
+        <tbody>
+        <tr v-for="item in itemInfo">
+          <th scope="row">1</th>
+          <td><img :src="item.image" :alt="item.productName"/></td>
+          <td>
+            Product Name: {{item.productName}}
+            Manufacturer: {{item.manufacturer}}
+            Description: {{item.description}}
+          </td>
+          <td>
+            {{item.quantity}}
+          </td>
+          <td>
+            {{item.price}}
+          </td>
+        </tr>
+        </tbody>
+      </table>
+    
+    
     </div>
     <!--<div v-for="item in itemInfo" class="itemInfo">-->
-      <!--&lt;!&ndash;description of item&ndash;&gt;-->
-      <!--{{item.description}}-->
+    <!--&lt;!&ndash;description of item&ndash;&gt;-->
+    <!--{{item.description}}-->
     <!--</div>-->
     <!--<div v-for="item in intemInfo" class="Quan">-->
-      <!--&lt;!&ndash;shows quanity of item&ndash;&gt;-->
-      <!--{{item.quantity}}-->
+    <!--&lt;!&ndash;shows quanity of item&ndash;&gt;-->
+    <!--{{item.quantity}}-->
     <!---->
     <!--</div>-->
     <!--<div v-for="item in intemInfo" class="Price">-->
-      <!--&lt;!&ndash;shows price of item&ndash;&gt;-->
-      <!--{{item.price}}-->
+    <!--&lt;!&ndash;shows price of item&ndash;&gt;-->
+    <!--{{item.price}}-->
     <!---->
     <!--</div>-->
     <!--<div class="subTotal">-->
-      <!--&lt;!&ndash;shows price of all items in cart&ndash;&gt;-->
+    <!--&lt;!&ndash;shows price of all items in cart&ndash;&gt;-->
     <!--</div>-->
   </div>
 </template>
