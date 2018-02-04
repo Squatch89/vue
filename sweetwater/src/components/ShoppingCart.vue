381c0f2ff590c5dbb0d64477ceae5bca103d77bd
Gene Williams <GeneWillb@Gmail.com>
2018-02-04 11:34:08 -0500

    test began, basic layout of cart

---
+++ sweetwater/src/components/ShoppingCart.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="cart">
+      <div class="items">
+        //image(that is a link to item), manufacturer ,product name, itemID,
+        
+      </div>
+      <div class="itemInfo">
+        //description of item
+        
+      </div>
+      <div class="Quan">
+        //shows quanity of item
+      </div>
+      <div class="Price">
+        //shows price of item
+        
+        
+      </div>
+      <div class="subTotal">
+        //shows price of all items in cart
+      </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "shopping-cart"
+    }
+</script>
+
+<style scoped>
+
+</style>
