b70ccc2b10640e6e0add3ab1fe5e0044092397cc
Gene Williams <GeneWillb@Gmail.com>
2018-02-04 12:13:01 -0500

    element itterated over per item in list, singular div with same data is now repeated

--- sweetwater/src/components/ShoppingCart.vue
+++ sweetwater/src/components/ShoppingCart.vue
@@ -2,30 +2,33 @@
   <div>
     <div v-for="item in itemInfo" class="cart">
       <!--image(that is a link to item), manufacturer ,product name, itemID,-->
-      
-      <img :src="item.image"/>
-      {{item.manufacturer}}
-      {{item.productName}}
-      {{item.itemId}}
-    
+      <div v-repeat="item in itemInfo" class="cartItem">
+        <img :src="item.image" :alt="item.productName"/>
+        {{item.manufacturer}}
+        {{item.productName}}
+        {{item.itemId}}
+        {{item.description}}
+        {{item.quantity}}
+        {{item.price}}
+      </div>
     </div>
-    <div v-for="item in itemInfo" class="itemInfo">
-      <!--description of item-->
-      {{item.description}}
-    </div>
-    <div v-for="item in intemInfo" class="Quan">
-      <!--shows quanity of item-->
-      {{item.quantity}}
-    
-    </div>
-    <div v-for="item in intemInfo" class="Price">
-      <!--shows price of item-->
-      {{item.price}}
-    
-    </div>
-    <div class="subTotal">
-      <!--shows price of all items in cart-->
-    </div>
+    <!--<div v-for="item in itemInfo" class="itemInfo">-->
+      <!--&lt;!&ndash;description of item&ndash;&gt;-->
+      <!--{{item.description}}-->
+    <!--</div>-->
+    <!--<div v-for="item in intemInfo" class="Quan">-->
+      <!--&lt;!&ndash;shows quanity of item&ndash;&gt;-->
+      <!--{{item.quantity}}-->
+    <!---->
+    <!--</div>-->
+    <!--<div v-for="item in intemInfo" class="Price">-->
+      <!--&lt;!&ndash;shows price of item&ndash;&gt;-->
+      <!--{{item.price}}-->
+    <!---->
+    <!--</div>-->
+    <!--<div class="subTotal">-->
+      <!--&lt;!&ndash;shows price of all items in cart&ndash;&gt;-->
+    <!--</div>-->
   </div>
 </template>
 
