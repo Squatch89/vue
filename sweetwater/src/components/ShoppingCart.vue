b384e4a00070b79079b28d444a0ddab2463bc318
Gene Williams <GeneWillb@Gmail.com>
2018-02-04 14:00:48 -0500

    table displaying items is setup, vuex is setup but unused

--- sweetwater/src/components/ShoppingCart.vue
+++ sweetwater/src/components/ShoppingCart.vue
@@ -1,87 +1,77 @@
 <template>
-  <div>
-    <div class="cart">
-      <!--image(that is a link to item), manufacturer ,product name, itemID,-->
-      <!--<div v-repeat="item in itemInfo" class="cartItem">-->
-      <!--<img :src="item.image" :alt="item.productName"/>-->
-      <!--{{item.manufacturer}}-->
-      <!--{{item.productName}}-->
-      <!--{{item.itemId}}-->
-      <!--{{item.description}}-->
-      <!--{{item.quantity}}-->
-      <!--{{item.price}}-->
-      </div>
-      
-      <table class="table">
-        <thead>
-        <tr>
-          <th scope="col">#</th>
-          <th scope="col">Item</th>
-          <th scope="col">Info</th>
-          <th scope="col">Quantity</th>
-          <th scope="col">Price</th>
-        </tr>
-        </thead>
-        <tbody>
-        <tr v-for="(item, index) in itemInfo">
-          <th scope="row">{{index + 1}}</th>
-          <td><img :src="item.image" :alt="item.productName"/></td>
-          <td>
-            Product Name: {{item.productName}}
-            <br/>
-            Manufacturer: {{item.manufacturer}}
+  <div class="cart">
+    
+    <table class="table">
+      <thead>
+      <tr>
+        <th scope="col">#</th>
+        <th scope="col">Item</th>
+        <th scope="col">Info</th>
+        <th scope="col">Quantity</th>
+        <th scope="col">Price</th>
+      </tr>
+      </thead>
+      <tbody>
+      <tr v-for="(item, index) in itemInfo">
+        <th scope="row">{{index + 1}}</th>
+        <td><img :src="item.image" :alt="item.productName"/></td>
+        <td>
+          Product Name: {{item.productName}}
           <br/>
-            Description: {{item.description}}
-          </td>
-          <td>
-            {{item.quantity}}
-          </td>
-          <td>
-            {{item.price}}
-          </td>
-        </tr>
-        <tr>
-          <td></td>
-          <td></td>
-          <td></td>
-          <td></td>
-          <td>{{$store.state.total}}</td>
-        </tr>
-        </tbody>
-      </table>
-    
-    
-    </div>
-    <!--<div v-for="item in itemInfo" class="itemInfo">-->
-    <!--&lt;!&ndash;description of item&ndash;&gt;-->
-    <!--{{item.description}}-->
-    <!--</div>-->
-    <!--<div v-for="item in intemInfo" class="Quan">-->
-    <!--&lt;!&ndash;shows quanity of item&ndash;&gt;-->
-    <!--{{item.quantity}}-->
-    <!---->
-    <!--</div>-->
-    <!--<div v-for="item in intemInfo" class="Price">-->
-    <!--&lt;!&ndash;shows price of item&ndash;&gt;-->
-    <!--{{item.price}}-->
-    <!---->
-    <!--</div>-->
-    <!--<div class="subTotal">-->
-    <!--&lt;!&ndash;shows price of all items in cart&ndash;&gt;-->
-    <!--</div>-->
+          Manufacturer: {{item.manufacturer}}
+          <br/>
+          Description: {{item.description}}
+        </td>
+        <td>
+          {{item.quantity}}
+          <!--add buttons to increase and decrease quantity-->
+          <!--have buttons check their current count against the quantity avaliable for each item-->
+          <!--do not allow more quantity to be added than there is avaliable-->
+          <!--if button count is === to item.quantity do not add any more-->
+          <!--if button count is < item.quantity you can add or subtract more-->
+          <!--if button count is === to 0 then do not remove any more but can add more-->
+        </td>
+        <td>
+          {{item.price}}
+        </td>
+      </tr>
+      <tr>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td>Subtotal: {{$store.state.total}}</td>
+        <!--update total to show the amount of all items added together taking into account their quantity-->
+      </tr>
+      </tbody>
+    </table>
   </div>
 </template>
 
 <script>
   import itemInfo from '../../items.json'
+  import {mapGetters} from 'vuex'
+  import {mapMutations} from 'vuex'
   
   export default {
     name: "ShoppingCart",
     data() {
       return {
         itemInfo,
-        total: this.$store.state.total
-      };
+        computed: {
+          ...mapGetters([
+            'getTotal'
+          ])
+        },
+        methods: {
+          increment() {
+            store.commit('increment', itemInfo.price)
+          },
+          decrement() {
+            store.commit('decrement', itemInfo.price)
+          }
+        }
+      }
     }
   }
 </script>
--- sweetwater/src/store/store.js
+++ sweetwater/src/store/store.js
@@ -1,12 +1,30 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 
-Vue.use(Vuex)
+Vue.use(Vuex);
 
 const state = {
   total: 0
-}
+};
+
+const getters = {
+  getTotal(state) {
+    return state.total;
+  }
+};
+
+const mutations = {
+  increment(state, payload) {
+    state.count += payload.price
+  
+  },
+  decrement(state, payload ) {
+    state.count -= payload.price
+  }
+};
 
 export default new Vuex.Store({
-  state
+  state,
+  getters,
+  mutations
 })
